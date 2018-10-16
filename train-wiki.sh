d3e0c03ca778dc48d0e97992d7d6d2ce5a27d41d
hellrich <johannes.hellrich@uni-jena.de>
2018-10-16 14:30:39 +0200

    fixed id in slurm out

--- train-wiki.sh
+++ train-wiki.sh
@@ -2,10 +2,17 @@
 
 CORPUS=wiki
 MIN=100
+WORKING_DIR=/home/hellrich/embedding_downsampling_comparison
+
+mkdir -p $WORKING_DIR/training_slurmout/
+
 for what in glove gloveb ppmi ppmib w2v w2vb
 do
 	for i in 0 # {0..9}
 	do
-		./train-slurm.sh $what $i $CORPUS $MIN
+		sbatch -o $WORKING_DIR/training_slurmout/${what}_$i train-slurm.sh $what $i $CORPUS $MIN
 	done
 done
+
+
+
