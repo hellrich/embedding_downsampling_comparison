5780f8daea69818bc9b4a70aed45a6b960cca41c
Johannes Hellrich <johannes.hellrich@uni-jena.de>
2019-03-26 10:08:16 +0100

    Update significance_tests.py
    Reprocessing after fixing MEN error

--- significance_tests.py
+++ significance_tests.py
@@ -38,7 +38,7 @@ auf dem DataFrame durch."""
     pval = 1 - f.cdf(stat, b-1, (b-1)*(k-1))
     return (stat, pval)
 
-data = open("significance_test_data")
+data = open("newdata_repeval.txt")
 header = data.readline().split()
 comparison = data.readlines()
 data.close()
@@ -60,7 +60,7 @@ for i in range(0,5):
 # Test auf sämtlichen Modellen
 scipy.stats.friedmanchisquare(*statarrays)
 #FriedmanchisquareResult(statistic=40.506329113924046, pvalue=3.400838407282329e-08)
-quade(pd.DataFrame(np.array(statarrays).T))
+print(quade(pd.DataFrame(np.array(statarrays).T)))
 #(10.701205306157464, 1.3494277528725007e-07)
 
 # Aus der scipy Dokumentation:
@@ -68,18 +68,22 @@ quade(pd.DataFrame(np.array(statarrays).T))
 # Because the normal approximation is used for the calculations,
 # the samples used should be large. A typical rule is to require that n > 20.
 
+#svd weight vs sgns
 _, p1 = scipy.stats.wilcoxon(statarrays[3], y=statarrays[4])
 # WilcoxonResult(statistic=214.5, pvalue=0.09973669447863137)
+#svd none vs svd weight
 _, p2 = scipy.stats.wilcoxon(statarrays[1], y=statarrays[3])
 # WilcoxonResult(statistic=26.5, pvalue=2.285205227112723e-06)
+#glove vs svd w
 _, p3 = scipy.stats.wilcoxon(statarrays[0], y=statarrays[3])
 # WilcoxonResult(statistic=155.5, pvalue=0.008987819337280168)
+#svd p vs svd w
 _, p4 = scipy.stats.wilcoxon(statarrays[2], y=statarrays[3])
 # WilcoxonResult(statistic=128.5, pvalue=0.00384145426183109)
 
 # p-Wert-Korrektur nach Holm-Šídák
 # http://www.statsmodels.org/dev/_modules/statsmodels/stats/multitest.html
-multitest.multipletests([p1,p2,p3,p4], method='hs')
+print(multitest.multipletests([p1,p2,p3,p4], method='hs'))
 #(
 # array([False,  True,  True,  True]),
 # array([9.97366945e-02, 9.14078958e-06, 1.78948578e-02, 1.14801492e-02]),
