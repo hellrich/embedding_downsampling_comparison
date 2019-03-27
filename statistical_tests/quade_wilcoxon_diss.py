c4a120409d326ae7851bb4d983623318d79c66d3
Johannes Hellrich <johannes.hellrich@uni-jena.de>
2019-03-27 14:13:18 +0100

    Update quade_wilcoxon_diss.py

--- statistical_tests/quade_wilcoxon_diss.py
+++ statistical_tests/quade_wilcoxon_diss.py
@@ -70,25 +70,18 @@ print(quade(pd.DataFrame(np.array(statarrays).T)))
 
 #svd weight vs sgns
 _, p1 = scipy.stats.wilcoxon(statarrays[3], y=statarrays[4])
-# WilcoxonResult(statistic=214.5, pvalue=0.09973669447863137)
 #svd none vs svd weight
 _, p2 = scipy.stats.wilcoxon(statarrays[1], y=statarrays[3])
-# WilcoxonResult(statistic=26.5, pvalue=2.285205227112723e-06)
 #glove vs svd w
 _, p3 = scipy.stats.wilcoxon(statarrays[0], y=statarrays[3])
-# WilcoxonResult(statistic=155.5, pvalue=0.008987819337280168)
 #svd p vs svd w
 _, p4 = scipy.stats.wilcoxon(statarrays[2], y=statarrays[3])
-# WilcoxonResult(statistic=128.5, pvalue=0.00384145426183109)
 
 print(p1,p2,p3,p4)
 # p-Wert-Korrektur nach Holm-Šídák
 # http://www.statsmodels.org/dev/_modules/statsmodels/stats/multitest.html
 print(multitest.multipletests([p1,p2,p3,p4], method='hs'))
-#(
-# array([False,  True,  True,  True]),
-# array([9.97366945e-02, 9.14078958e-06, 1.78948578e-02, 1.14801492e-02]),
-# 0.012741455098566168, 0.0125)
+
 
 # Das erste Array gibt an, welche Null-Hypothesen für das gegebene alpha verworfen werden können
 # Das zweite Array gibt die korrigierten p-Werte an
